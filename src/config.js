88816e8e4993981fba4464bbc341b4bdd36fd5fc
TheExtremezCoder <156786126+TheExtremezCoder@users.noreply.github.com>
2024-04-10 19:50:28 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 
 module.exports = {
-  token: process.env.TOKEN || 'MTIyMDA0MzAyNDU1NDUyNDY5Mg.GXdfRF.Zrtjgc8NzMhipm1SGCPsVL-8M2rUPMD9EVuIys', // discord bot token
+  token: process.env.TOKEN || '', // discord bot token
   prefix: process.env.PREFIX || ",", 
-  ownerID: process.env.OWNERID || ['1173547185758015498'], //your discord id
-  premiumID: process.env.PREMIUMID || ['1173547185758015498'],
+  ownerID: process.env.OWNERID || [''], //your discord id
+  premiumID: process.env.PREMIUMID || [''],
   SpotifyID: process.env.SPOTIFYID || '606ae4545f3f4994a47275d1d6f94261', // spotify client id
   SpotifySecret: process.env.SPOTIFYSECRET || '0a3e58753ea641a18ebc0f4ea6320176', // spotify client secret
   mongourl: process.env.MONGO_URI || 'mongodb+srv://modsa:modsa_db@modsadev.dcaruzy.mongodb.net/?retryWrites=true&w=majority', // MongoDb URL
   embedColor: process.env.COlOR || '#2f3136', // embed colour
-  logs: process.env.LOGS || '1220258349334007808', // Discord channel id 
+  logs: process.env.LOGS || '', // Discord channel id 
   links: {
     support: process.env.SUPPORT || 'https://discord.gg/YFUr6FDw',
     invite: process.env.INVITE || 'https://discord.com/api/oauth2/authorize?client_id=1027811949792219196&permissions=8&scope=bot',
     vote: process.env.VOTE || 'https://discord.gg/instance',
     bg: process.env.BG || 'https://media.discordapp.net/attachments/989120751418343465/989476878421200916/Wofi_Music.png?width=771&height=434',
-    premium: ['1173547185758015498']
+    premium: ['']
   },
 
   nodes: [
